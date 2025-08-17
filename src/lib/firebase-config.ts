4096e8ebe8303af863403a6bbd9c415ec4fbf2c8
7K MIND <7kmindbeatss@gmail.com>
2025-08-17 07:12:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "lawprep-sprint",
-  "appId": "1:633482539204:web:8bb00b05226af3faaf1991",
-  "storageBucket": "lawprep-sprint.firebasestorage.app",
   "apiKey": "AIzaSyAomgxhOBMMW17eAgoNkl05hQyOEYevYDs",
   "authDomain": "lawprep-sprint.firebaseapp.com",
+  "projectId": "lawprep-sprint",
+  "storageBucket": "lawprep-sprint.appspot.com",
   "messagingSenderId": "633482539204",
+  "appId": "1:633482539204:web:8bb00b05226af3faaf1991",
   "measurementId": "G-4J7C6J7C6J"
 };
 
